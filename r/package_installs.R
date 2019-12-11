899aea359ee5f2559f5eca28c7ff57fb112be170
jesnstevens <21069023+jesnstevens@users.noreply.github.com>
2019-12-11 11:40:49 -0600

    updates to software documentation

--- r/package_installs.R
+++ r/package_installs.R
@@ -1,8 +1,19 @@
+library(here)
+
 #Get currently installed packages
 package_df <- as.data.frame(installed.packages())
 package_list <- as.character(package_df$Package)
 
-#Re-install Install packages
+# Print Package List
+package_list
+
+# Saves list of installed packages
+save(package_list, package_df, file = here("r", "ipl.rda"))
+
+# Loads list of remotely installed packages
+load(file = here("r", "ipl.rda"))
+
+# Re-install packages
 install.packages(package_list)
 
 #Packages Installed from Github
--- r/package_list.R
+++
@@ -1,8 +0,0 @@
-ip = as.data.frame(installed.packages()[,c(1,3:4)])
-
-ip = ip[is.na(ip$Priority),1:2,drop=FALSE]
-
-ip
-
-# Saves list of installed packages
-save(ip, file = "ipl.rda")
